4fd6f8708f592411888a76763797c7c1b4a6be54
VwoLFy <v_wolfy@mail.ru>
2022-07-28 17:28:01 +0300

    f83

---
+++ f83.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Dik",
+    legs: 4,
+    tails: 1,
+    friends: ["men", "dogs"]
+  
+    // Only change code above this line
+  };
