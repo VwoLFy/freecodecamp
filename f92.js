c849922985d99b7918287d743080da853aec3424
VwoLFy <v_wolfy@mail.ru>
2022-07-28 18:39:58 +0300

    f92

---
+++ f92.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Bon Jovi",
+      "title": "The Best",
+      "release_year": 1994,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+    }
+  ];
